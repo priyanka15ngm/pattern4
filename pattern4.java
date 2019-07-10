f574183f6c352080f6747f28c5b73ebfbb2b3a3b
priyanka nigam <45058302+priyanka15ngm@users.noreply.github.com>
2019-07-10 10:40:11 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,23 @@
+class Pattern4
+{ 
+	
+
+	public static void main(String[] args) 
+	{
+		int i;
+		int j;
+		int k=0;
+
+		for(i=1;i<=5;i++);
+		{
+			for(j=1;j<=i;j++)
+			{
+				
+				System.out.print(k);
+			}
+			++k;
+			System.out.println();
+		}
+				
+	}
+}
